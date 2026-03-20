bdb7453e5a8effea56073a0b3eaa192a173a438c
nishchay28 <bansalnishchay28@gmail.com>
2026-03-20 08:22:41 +0530

    UC2: Generalized Room via Abstract Class

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,7 +1,63 @@
+// Abstract class representing a generalized Room
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: $" + price);
+    }
+}
+
+// Concrete room classes
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1000.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 1800.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 3500.0);
+    }
+}
+
 public class BookMyStayApp {
     public static void main(String[] args) {
         System.out.println("Welcome to BookMyStayApp");
-        System.out.println("Version: 1.0");
+        System.out.println("Version: 2.0");
         System.out.println("Application started successfully.");
+
+        // Availability variables (static representation)
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        // Create room objects
+        Room single = new SingleRoom();
+        Room doubleR = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Display room details and availability
+        single.displayDetails();
+        System.out.println("Available: " + singleRoomAvailability);
+
+        doubleR.displayDetails();
+        System.out.println("Available: " + doubleRoomAvailability);
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteRoomAvailability);
     }
 }
